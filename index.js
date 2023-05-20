21dce5f7a76c326c75709ec6284c1d7002e9af8e
Amrit Jhatu <a_jhatu@hotmail.com>
2023-05-20 10:38:18 -0700

    Steps 7 8 9 inserted.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+let isFlipping = false;
+
 const setup = () => {
     let firstCard = undefined;
     let secondCard = undefined;
 
     $(".card").on("click", function () {
+        // Ignore clicks if game is currently flipping cards back
+        if (isFlipping) {
+            return;
+        }
+
+        // Ignore double clicks on the same card
         if (firstCard && firstCard === $(this).find(".front_face")[0]) {
             return;
         }
 
         $(this).toggleClass("flip");
-    
+
         if (!firstCard) {
             firstCard = $(this).find(".front_face")[0];
         } else {
@@ -19,15 +27,19 @@ const setup = () => {
                 console.log("match");
                 $(`#${firstCard.id}`).parent().off("click");
                 $(`#${secondCard.id}`).parent().off("click");
+                firstCard = undefined;
+                secondCard = undefined;
             } else {
                 console.log("no match");
+                isFlipping = true;
                 setTimeout(() => {
                     $(`#${firstCard.id}`).parent().toggleClass("flip");
                     $(`#${secondCard.id}`).parent().toggleClass("flip");
+                    firstCard = undefined;
+                    secondCard = undefined;
+                    isFlipping = false;
                 }, 1000);
             }
-            firstCard = undefined;
-            secondCard = undefined;
         }
     });
 }
