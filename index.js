500a80b42c6d62497de8c22792c180f2f62afb93
Amrit Jhatu <a_jhatu@hotmail.com>
2023-05-23 01:04:56 -0700

    power.

--- index.js
+++ index.js
@@ -6,33 +6,40 @@ let totalPairs = 3;
 let timer = undefined;
 let timerCount = 0;
 
-$('#difficulty').change(() => {
-  const difficulty = $('#difficulty').val();
-  totalPairs = difficulty;
-  resetGame();
-});
+// Implementation of resetGame function
+const resetGame = () => {
+  // You need to write your own implementation for this
+  // Reset variables
+  clickCount = 0;
+  matchedPairs = 0;
+  timerCount = 0;
+  // Reset game state
+  // For example, un-flip all the cards, reshuffle, etc.
+  $('.card').removeClass('flip');
+  updateStats();
+};
 
-$('#theme').change(() => {
-  const theme = $('#theme').val();
-  $('body').removeClass('light dark').addClass(theme);
-});
+// Implementation of startTimer function
+const startTimer = () => {
+  // Starting the timer
+  timer = setInterval(() => {
+    timerCount++;
+    $('#timer').text(timerCount + " Seconds");
+  }, 1000);
+};
 
-$('#reveal-button').click(() => {
-  $('.card').addClass('flip');
-  $("#powerup-notification").text("Power Up Active! Cards are revealed for 3 seconds.");
-  setTimeout(() => {
-    $('.card').removeClass('flip');
-    $("#powerup-notification").text("");
-  }, 3000);
-});
+// Stop timer function
+const stopTimer = () => {
+  clearInterval(timer);
+};
 
-$('#reset-button').click(resetGame);
-
-$('#start-button').click(() => {
-  resetGame();
-  startTimer();
-});
-
+// Update game statistics
+const updateStats = () => {
+  $('#click-count').text(clickCount);
+  $('#matched-pairs').text(matchedPairs);
+  $('#total-pairs').text(totalPairs);
+  $('#pairs-left').text(totalPairs - matchedPairs);
+};
 
 const setup = () => {
   $(".card").on("click", function () {
@@ -70,4 +77,33 @@ const setup = () => {
   });
 };
 
-$(document).ready(setup);
+$(document).ready(() => {
+  $('#difficulty').change(() => {
+    const difficulty = $('#difficulty').val();
+    totalPairs = difficulty;
+    resetGame();
+  });
+
+  $('#theme').change(() => {
+    const theme = $('#theme').val();
+    $('body').removeClass('light dark').addClass(theme);
+  });
+
+  $('#reveal-button').click(() => {
+    $('.card').addClass('flip');
+    $("#powerup-notification").text("Power Up Active! Cards are revealed for 3 seconds.");
+    setTimeout(() => {
+      $('.card').removeClass('flip');
+      $("#powerup-notification").text("");
+    }, 3000);
+  });
+
+  $('#reset-button').click(resetGame);
+
+  $('#start-button').click(() => {
+    resetGame();
+    startTimer();
+  });
+  
+  setup();
+});
