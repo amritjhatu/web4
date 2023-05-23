977d80631c3714a7097dbcaaa139d09b320ac65c
Amrit Jhatu <a_jhatu@hotmail.com>
2023-05-23 00:35:39 -0700

    style changed.

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 let clickCount = 0;
 let matchedPairs = 0;
+let firstCard = undefined;
+let secondCard = undefined;
+let timeElapsed = 0;
+let timerInterval;
 const totalPairs = 3; // Change this if you add more pairs
+
+const startTimer = () => {
+  timerInterval = setInterval(() => {
+    timeElapsed++;
+    updateStats();
+  }, 1000);
+};
+
+const stopTimer = () => {
+  clearInterval(timerInterval);
+};
 
 const updateStats = () => {
   $('#click-count').text(clickCount);
   $('#matched-pairs').text(matchedPairs);
   $('#total-pairs').text(totalPairs);
-  $('#pairs-left').text(totalPairs - matchedPairs); // Number of pairs left
+  $('#pairs-left').text(totalPairs - matchedPairs);
+  $('#timer').text(timeElapsed);
 };
 
-const setup = () => {
-  let firstCard = undefined;
-  let secondCard = undefined;
+const resetGame = () => {
+  stopTimer();
+  timeElapsed = 0;
+  clickCount = 0;
+  matchedPairs = 0;
+  firstCard = undefined;
+  secondCard = undefined;
+  $('.card').removeClass('flip').on('click');
+  updateStats();
+};
 
+$('#reset-button').click(resetGame);
+
+$('#start-button').click(() => {
+  resetGame();
+  startTimer();
+});
+
+const setup = () => {
   $(".card").on("click", function () {
     if (secondCard) return;
     if (firstCard && $(this).find(".front_face")[0].id === firstCard.id) return;
@@ -32,6 +63,7 @@ const setup = () => {
         $(`#${secondCard.id}`).parent().off("click");
         if (matchedPairs === totalPairs) {
           alert('You won!');
+          stopTimer();
         }
         firstCard = undefined;
         secondCard = undefined;
