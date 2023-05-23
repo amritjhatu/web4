554850db461873d0f3f487980f9101e687677d15
Amrit Jhatu <a_jhatu@hotmail.com>
2023-05-23 01:45:58 -0700

    ch.

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const updateStats = () => {
 const setup = () => {
   $(".card").on("click", function () {
     if (secondCard) return;
-    if (firstCard && $(this).find(".front_face img").attr("id") === firstCard.id) return;
+    if (firstCard && $(this).find(".front_face")[0].id === firstCard.id) return;
 
     $(this).toggleClass("flip");
     clickCount++;
@@ -78,12 +78,12 @@ const setup = () => {
 };
 
 $(document).ready(() => {
-    $('#difficulty').change(() => {
-        const difficulty = $('#difficulty').val();
-        totalPairs = parseInt(difficulty);
-        resetGame();
-      });
-      
+$('#difficulty').change(() => {
+  const difficulty = $('#difficulty').val();
+  totalPairs = parseInt(difficulty);
+  resetGame();
+});
+
 
   $('#theme').change(() => {
     const theme = $('#theme').val();
