67b88bcaaf674d58b0b0c00e3a901b8092c29171
Amrit Jhatu <a_jhatu@hotmail.com>
2023-05-23 00:10:45 -0700

    change.

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-let isFlipping = false;
+
+
 
 const setup = () => {
-    let firstCard = undefined;
-    let secondCard = undefined;
-
-    $(".card").on("click", function () {
-        // Ignore clicks if game is currently flipping cards back
-        if (isFlipping) {
-            return;
-        }
-
-        // Ignore double clicks on the same card
-        if (firstCard && firstCard === $(this).find(".front_face")[0]) {
-            return;
-        }
-
-        $(this).toggleClass("flip");
-
-        if (!firstCard) {
-            firstCard = $(this).find(".front_face")[0];
+    let firstCard = undefined
+    let secondCard = undefined
+    $(".card").on(("click"), function () {
+      $(this).toggleClass("flip");
+  
+      if (!firstCard)
+        firstCard = $(this).find(".front_face")[0]
+      else {
+        secondCard = $(this).find(".front_face")[0]
+        console.log(firstCard, secondCard);
+        if (
+          firstCard.src
+          ==
+          secondCard.src
+        ) {
+          console.log("match")
+          $(`#${firstCard.id}`).parent().off("click")
+          $(`#${secondCard.id}`).parent().off("click")
         } else {
-            secondCard = $(this).find(".front_face")[0];
-            console.log(firstCard, secondCard);
-
-            if (firstCard.src === secondCard.src) {
-                console.log("match");
-                $(`#${firstCard.id}`).parent().off("click");
-                $(`#${secondCard.id}`).parent().off("click");
-                firstCard = undefined;
-                secondCard = undefined;
-            } else {
-                console.log("no match");
-                isFlipping = true;
-                setTimeout(() => {
-                    $(`#${firstCard.id}`).parent().toggleClass("flip");
-                    $(`#${secondCard.id}`).parent().toggleClass("flip");
-                    firstCard = undefined;
-                    secondCard = undefined;
-                    isFlipping = false;
-                }, 1000);
-            }
+          console.log("no match")
+          setTimeout(() => {
+            $(`#${firstCard.id}`).parent().toggleClass("flip")
+            $(`#${secondCard.id}`).parent().toggleClass("flip")
+          }, 1000)
         }
+      }
     });
-}
-
-$(document).ready(setup);
+  }
+  
+  $(document).ready(setup)
